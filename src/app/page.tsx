13bc18c1816a934d94cf3eb83ee64153a52bc8af
lucasdksan <lucas.leoncio.silva@gmail.com>
2025-04-20 00:48:43 -0300

    feat: modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import { FormModal } from "@/components/form-modal-component";
 
 export default function Home() {
   return (
     <main className="w-screen h-screen md:flex md:items-center md:justify-center">
       <div className="container mx-auto flex flex-col p-3 gap-6 md:flex-row md:items-center md:justify-center md:gap-9">
-        <div className="gap-6 flex flex-col md:max-w-[400px] md:gap-4">
+        <div className="gap-6 flex flex-col md:gap-4">
           <h1 className="font-roboto not-italic font-bold text-4xl md:text-5xl text-white">Seu valor de hora, calculado de forma rápida.</h1>
           <p className="font-roboto not-italic font-normal text-base text-white">
             Descubra quanto vale o seu trabalho de forma rápida, precisa e sem complicações. <br />
@@ -12,18 +13,10 @@ export default function Home() {
           </p>
           <div className="w-full flex gap-3 items-center">
             <span className="font-roboto not-italic font-normal text-base text-white">Vamos iniciar?</span>
-            <button arial-label="Iniciar o processo" className="bg-btn-background flex gap-3 p-2 rounded-lg cursor-pointer">
-              <span className="font-roboto not-italic font-medium text-base text-white">Free</span>
-              <Image
-                src="/arrow-right.svg"
-                width={24}
-                height={24}
-                alt="Arrow icon to start process"
-              />
-            </button>
+            <FormModal />
           </div>
         </div>
-        <div className="w-full h-auto lg:max-w-[485px]">
+        <div className="w-full h-auto lg:max-w-[520px]">
           <Image
             src="/tab-banner.svg"
             width={420}
---
+++ src/components/form-modal-component.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import Image from "next/image";
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
+export function FormModal() {
+    const [openForm, setOpenForm] = useState(false);
+
+    const handleToggleModal = (event: React.MouseEvent<HTMLDivElement>) => {
+        event.stopPropagation();
+        setOpenForm((prev) => !prev);
+    }
+
+    return (
+        <>
+            <button onClick={() => setOpenForm(true)} arial-label="Iniciar o processo" className="bg-btn-background flex gap-3 p-2 rounded-lg cursor-pointer">
+                <span className="font-roboto not-italic font-medium text-base text-white">Grátis</span>
+                <Image
+                    src="/arrow-right.svg"
+                    width={24}
+                    height={24}
+                    alt="Arrow icon to start process"
+                />
+            </button>
+            { openForm && createPortal(
+                <>
+                    <div onClick={handleToggleModal} className="w-screen h-screen flex items-center justify-center absolute top-0 left-0 z-10 bg-modal-background">
+                    
+                    </div>
+                    <div className="absolute top-1/2 left-1/2 z-30 bg-form-background w-10 h-10 transform -translate-x-1/2 -translate-y-1/2">
+    
+                    </div>
+                </>,
+                document.body
+            ) }
+        </>
+    );
+}
