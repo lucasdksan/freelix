df9b82031271746f9118e2b7b2af6a270dceedfa
lucasdksan <lucas.leoncio.silva@gmail.com>
2025-04-23 23:59:28 -0300

    fix: melhoria no arquivo

--- src/app/api/generate-proposal/route.ts
+++ src/app/api/generate-proposal/route.ts
@@ -18,30 +18,53 @@ export async function POST(req: Request) {
             {
                 properties: {},
                 children: [
-                    new Paragraph(`${cite}, ${day} de ${mouth} de ${year}`),
-                    new Paragraph(""),
-                    new Paragraph("Proposta Comercial"),
-                    new Paragraph(`A\n ${nameCompany}, \n ${partners}`),
-                    new Paragraph(""),
+                    new Paragraph({ text: `${cite}, ${day} de ${mouth} de ${year}`, spacing: { after: 300 } }),
+                    new Paragraph({ text: "Proposta Comercial", spacing: { after: 300 }, heading: "Heading1" }),
+                    new Paragraph({ text: "A", spacing: { after: 100 } }),
+                    new Paragraph({ text: nameCompany, spacing: { after: 100 } }),
+                    new Paragraph({ text: partners, spacing: { after: 300 } }),
+    
                     new Paragraph("Prezados,"),
-                    new Paragraph(descriptionProducts),
-                    new Paragraph("Desde já agradecemos a oportunidade"),
+                    new Paragraph({
+                        text: descriptionProducts,
+                        spacing: { after: 300 }
+                    }),
+                    new Paragraph("Desde já agradecemos a oportunidade e nos colocamos à disposição para quaisquer esclarecimentos."),
                     new Paragraph(""),
-                    new Paragraph(`Atenciosamente, \n ${name} - Diretor`),
+                    new Paragraph({ text: `Atenciosamente,`, spacing: { after: 100 } }),
+                    new Paragraph({ text: `${name} - Diretor`, spacing: { after: 300 }}),
+    
+                    new Paragraph({ text: "SUMÁRIO", heading: "Heading2", spacing: { after: 200 } }),
+                    new Paragraph("Introdução ________________________________________________ 2"),
+                    new Paragraph("Argumentação ____________________________________________ 2"),
+                    new Paragraph("Escopo ___________________________________________________ 3"),
+                    new Paragraph("Cronograma ______________________________________________ 3"),
+                    new Paragraph("Valor dos serviços ________________________________________ 3"),
                     new Paragraph(""),
-                    new Paragraph("1. Introdução"),
+    
+                    new Paragraph({ text: "1. Introdução", heading: "Heading2" }),
                     new Paragraph(introduction),
-                    new Paragraph("2. Escopo"),
+    
+                    new Paragraph({ text: "2. Argumentação", heading: "Heading2" }),
+                    new Paragraph("O objetivo desta proposta é apresentar os preços e prazos dos serviços outrora requeridos."),
+    
+                    new Paragraph({ text: "3. Escopo", heading: "Heading2" }),
                     new Paragraph(scope),
-                    new Paragraph("3. Prazo"),
+    
+                    new Paragraph({ text: "4. Cronograma", heading: "Heading2" }),
                     new Paragraph(term),
-                    new Paragraph("4. Valor dos Serviços"),
+    
+                    new Paragraph({ text: "5. Valor dos Serviços", heading: "Heading2" }),
                     new Paragraph(value),
-                    new Paragraph("5. Bônus de Contratação"),
-                    new Paragraph("Suporte 24h gratuito no 1º ano..."),
-                    new Paragraph("Configuração gratuita nas lojas..."),
-                    new Paragraph("De Acordo Comercial"),
+    
+                    new Paragraph({ text: "6. Bônus de Contratação", heading: "Heading2" }),
+                    new Paragraph("Suporte 24h gratuito para quaisquer bugs ou problemas no primeiro ano de existência do sistema via grupo no WhatsApp com os desenvolvedores."),
+                    new Paragraph("Configuração gratuita de conta no Google Play Store Console e App Store Console."),
+                    new Paragraph("Vigilância de disponibilidade 24h com backup a cada 1 hora..."),
+    
+                    new Paragraph({ text: "De Acordo Comercial", spacing: { before: 400, after: 100 } }),
                     new Paragraph(""),
+                    new Paragraph("___________________________________________________________"),
                     new Paragraph(name),
                     new Paragraph(`${address} | ${phone}, ${signatureDate}`),
                 ]
