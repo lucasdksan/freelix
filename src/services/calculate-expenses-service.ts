c32571bb3f6138f6ecdba526bc79654c240dfb73
lucasdksan <lucas.leoncio.silva@gmail.com>
2025-04-25 17:33:30 -0300

    docs: adicionando novas documentações

--- src/services/calculate-expenses-service.ts
+++ src/services/calculate-expenses-service.ts
@@ -20,11 +20,11 @@ export function calculateExpenses({
     otherExpenses = 0
 }: ExpensesInput) {
     const thirteenthSalary = monthlyExpenses / 12;
-    const vacationBonus = (monthlyExpenses / 12) * (1/3);
+    const vacationBonus = thirteenthSalary * (1/3);
     const fixedCosts = monthlyExpenses + thirteenthSalary + vacationBonus
         + softwareLicenses + healthInsurance + accountantFees + otherExpenses; 
     const totalWithTaxes = fixedCosts * (1 + (taxesPercentage/100));
-    const totalWithProfit =totalWithTaxes * (1 + (desiredProfitMargin/100));
+    const totalWithProfit = totalWithTaxes * (1 + (desiredProfitMargin/100));
     const hourlyRate = totalWithProfit / workingHoursPerMonth;
 
     return Math.round(hourlyRate);
